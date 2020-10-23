81f3a0b147d94e7f442ad08e18bfae98a0475587
yosh-0827 <73326181+yosh-0827@users.noreply.github.com>
2020-10-23 13:43:22 +0900

    initial commit
    インターネット上のHTMLファイルを読み込んでテキストファイルでダウンロード

---
+++ chap1-2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import requests
+
+url = "https://www.ymori.com/books/python2nen/test1.html"
+response = requests.get(url)
+response.encoding = response.apparent_encoding
+
+filename = "download.txt"
+f = open(filename, "w")
+
+f.write(response.text)
+f.close()
+
+
+# In[ ]:
+
+
+
+
